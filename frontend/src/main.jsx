67679423ef2e82fd08f2ab09d4874beb07bb8c4e
Sachin Mishra <sachin2005mishra@gmail.com>
2026-02-17 13:16:53 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {Provider}  from "react-redux"
 import {store} from "./redux/store.js"
-export const serverUrl="http://localhost:5000"
+export const serverUrl="https://real-time-chat-backend-46xn.onrender.com"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
