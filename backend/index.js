ef1610763ee1f8e51511712dadc71dc0c40fa382
Sachin Mishra <sachin2005mishra@gmail.com>
2026-02-17 13:21:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const port=process.env.PORT || 8000;
 
 
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://chatly-az01.onrender.com",
   credentials:true
 }))
 app.use(express.json())
